ac55f1ac08e2404b35654144d80420964c28d8bd
dlkst14 <85811551+dlkst14@users.noreply.github.com>
2021-06-27 18:57:32 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+//DECLARAR FUNCIONES
+void ingresar (int arreglo1[], int n);
+void ordenar (int arreglo1[], int n);
+void imprimir (int arreglo1[], int n);
+
+//LLAMAR A LAS FUNCIONES
+int main()
+{
+    int arreglo1[5];
+    ingresar(arreglo1,5);
+    ordenar(arreglo1,5);
+    imprimir(arreglo1,5);
+    return 0;
+}
+
+//DESARROLLAR FUNCIONES
+void ingresar (int arreglo1[], int n)
+{
+    cout<<"Ingresar los elementos al arreglo:"<<endl;
+    for (int i=0 ; i<n ; i++)//definiremos variables dentro del for
+    {
+        cin>>arreglo1[i];
+    }
+}
+void ordenar (int arreglo1[], int n)
+{
+    int aux;
+    for (int i=0 ; i<n ; i++)
+    {
+        for (int j=i+1 ; j<n ; j++)
+        {
+            if(arreglo1[j]<arreglo1[i])
+            {
+                aux=arreglo1[i];//Se guarda momentaneamente
+                arreglo1[i]=arreglo1[j];
+                arreglo1 [j]=aux;
+            }
+        }
+    }
+}
+void imprimir (int arreglo1[], int n)
+{
+    int i; //Declarando la variable i fuera del for
+    for (i=0 ; i<n ; i++)
+    {
+        cout<<"|"<<arreglo1[i]<<"|";
+    }
+}
