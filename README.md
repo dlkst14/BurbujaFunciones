71e0b2f15b9638ad6c2e341820a2cddfc20b9715
dlkst14 <85811551+dlkst14@users.noreply.github.com>
2021-06-27 22:25:24 +0000

    Algoritmo-de-seleccionFunc

--- README.md
+++
@@ -1,4 +0,0 @@
-# BurbujaFunciones
-practica con funciones
-##Subtitulo
-- ***Prueba de github en negrita***
--- main.cpp
+++ main.cpp
@@ -1,52 +1,45 @@
 #include <iostream>
 using namespace std;
+//DECLARAMOS FUNCIONES
+void ordenar(int arreglo[],int n);
 
-//DECLARAR FUNCIONES
-void ingresar (int arreglo1[], int n);
-void ordenar (int arreglo1[], int n);
-void imprimir (int arreglo1[], int n);
-
-//LLAMAR A LAS FUNCIONES
+//LLAMADO DE LA FUNCION
 int main()
 {
-    int arreglo1[5];
-    ingresar(arreglo1,5);
-    ordenar(arreglo1,5);
-    imprimir(arreglo1,5);
+    int arreglo[5]= {2,3,1,5,4};
+    int i,j,aux,min;
+    //Arreglo original
+    cout<<"Arreglo original:";
+    for (i=0;i<5;i++)
+    {
+        cout<<"|"<<arreglo[i]<<"|";
+    }
+    cout<<endl;
+    ordenar(arreglo,5);
     return 0;
 }
-
-//DESARROLLAR FUNCIONES
-void ingresar (int arreglo1[], int n)
+//CONSTRUCCIONDE  FUNCIONES
+void ordenar(int arreglo[],int n)
 {
-    cout<<"Ingresar los elementos al arreglo:"<<endl;
-    for (int i=0 ; i<n ; i++)//definiremos variables dentro del for
+    cout<<"Arreglo ordenado por seleccion:";
+    int i,j,aux,min;
+    for (i=0;i<5;i++)
     {
-        cin>>arreglo1[i];
-    }
-}
-void ordenar (int arreglo1[], int n)
-{
-    int aux;
-    for (int i=0 ; i<n ; i++)
-    {
-        for (int j=i+1 ; j<n ; j++)
+        min = i; //se guardara temporalmente
+        for(j=i+1;j<5;j++ )
         {
-            if(arreglo1[j]<arreglo1[i])
+            if(arreglo[j]<arreglo[min])
             {
-                aux=arreglo1[i];//Se guarda momentaneamente
-                arreglo1[i]=arreglo1[j];
-                arreglo1 [j]=aux;
+                min=j;
             }
         }
+        aux=arreglo[i]; //se guardara temporalmente
+        arreglo[i]=arreglo[min];//se actualiza el guardado
+        arreglo[min]=aux;
     }
-}
-void imprimir (int arreglo1[], int n)
-{
-    int i; //Declarando la variable i fuera del for
-    for (i=0 ; i<n ; i++)
+    for(i=0;i<5;i++)  // esto tambien podemos escribirlo en el main, pero con return
     {
-        cout<<"|"<<arreglo1[i]<<"|";
+        cout<<"|"<<arreglo[i]<<"|";
     }
+    //return 0;  NO se usa "return" en la construccion con void
 }
-//PROBANDO BOTON DE + PARA ACTUALIZAR EN GITHUB
